b2f7b8173f49dc8ca4b52275d807caf8bc31a9fa
Teurowel <teurowel@gmail.com>
2020-10-10 15:12:07 +0900

    Update unity version Added comments for server, client

--- Assets/Scripts/NetworkClient.cs
+++ Assets/Scripts/NetworkClient.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 public class NetworkClient : MonoBehaviour
 {
-    public NetworkDriver m_Driver;
+    public NetworkDriver m_Driver; //simliar to socket
     public NetworkConnection m_Connection;
     public string serverIP;
     public ushort serverPort;
@@ -16,89 +16,55 @@ public class NetworkClient : MonoBehaviour
     
     void Start ()
     {
-        m_Driver = NetworkDriver.Create();
+        m_Driver = NetworkDriver.Create(); //creat socket
+
         m_Connection = default(NetworkConnection);
-        var endpoint = NetworkEndPoint.Parse(serverIP,serverPort);
-        m_Connection = m_Driver.Connect(endpoint);
-    }
-    
-    void SendToServer(string message){
-        var writer = m_Driver.BeginSend(m_Connection);
-        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message),Allocator.Temp);
-        writer.WriteBytes(bytes);
-        m_Driver.EndSend(writer);
-    }
 
-    void OnConnect(){
-        Debug.Log("We are now connected to the server");
-
-        //// Example to send a handshake message:
-        // HandshakeMsg m = new HandshakeMsg();
-        // m.player.id = m_Connection.InternalId.ToString();
-        // SendToServer(JsonUtility.ToJson(m));
-    }
-
-    void OnData(DataStreamReader stream){
-        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
-        stream.ReadBytes(bytes);
-        string recMsg = Encoding.ASCII.GetString(bytes.ToArray());
-        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg);
-
-        switch(header.cmd){
-            case Commands.HANDSHAKE:
-            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
-            Debug.Log("Handshake message received!");
-            break;
-            case Commands.PLAYER_UPDATE:
-            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>(recMsg);
-            Debug.Log("Player update message received!");
-            break;
-            case Commands.SERVER_UPDATE:
-            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>(recMsg);
-            Debug.Log("Server update message received!");
-            break;
-            default:
-            Debug.Log("Unrecognized message received!");
-            break;
-        }
-    }
-
-    void Disconnect(){
-        m_Connection.Disconnect(m_Driver);
-        m_Connection = default(NetworkConnection);
-    }
-
-    void OnDisconnect(){
-        Debug.Log("Client got disconnected from server");
-        m_Connection = default(NetworkConnection);
+        var endpoint = NetworkEndPoint.Parse(serverIP,serverPort); 
+        m_Connection = m_Driver.Connect(endpoint); //connect to server
     }
 
     public void OnDestroy()
     {
+        //Both NetworkDriver and NativeList allocate unmanaged memory and need to be disposed. 
+        //To make sure this happens we can simply call the Dispose method when we are done with both of them.
         m_Driver.Dispose();
-    }   
+    }
     void Update()
     {
+        //You start the same way as you did in the server by calling m_Driver.ScheduleUpdate().Complete(); and make sure that the connection worked.
         m_Driver.ScheduleUpdate().Complete();
 
+
+
+
+        //Check if connection is alive
         if (!m_Connection.IsCreated)
         {
             return;
         }
 
-        DataStreamReader stream;
-        NetworkEvent.Type cmd;
-        cmd = m_Connection.PopEvent(m_Driver, out stream);
+
+
+
+
+        DataStreamReader stream; //Where to store data
+        NetworkEvent.Type cmd; //To check what data this is
+
+        cmd = m_Connection.PopEvent(m_Driver, out stream); //Get data from socket
         while (cmd != NetworkEvent.Type.Empty)
         {
+            //This event tells you that you have received a ConnectionAccept message and you are now connected to the remote peer.
             if (cmd == NetworkEvent.Type.Connect)
             {
                 OnConnect();
             }
+            //If you get data from server...
             else if (cmd == NetworkEvent.Type.Data)
             {
                 OnData(stream);
             }
+            //Lastly we just want to make sure we handle the case that a server disconnects us for some reason.
             else if (cmd == NetworkEvent.Type.Disconnect)
             {
                 OnDisconnect();
@@ -107,4 +73,70 @@ public class NetworkClient : MonoBehaviour
             cmd = m_Connection.PopEvent(m_Driver, out stream);
         }
     }
+
+    void OnConnect()
+    {
+        Debug.Log("We are now connected to the server");
+
+        //// Example to send a handshake message:
+        // HandshakeMsg m = new HandshakeMsg();
+        // m.player.id = m_Connection.InternalId.ToString();
+        // SendToServer(JsonUtility.ToJson(m));
+    }
+
+
+    void OnData(DataStreamReader stream)
+    {
+        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
+        stream.ReadBytes(bytes); //Get bytes
+        string recMsg = Encoding.ASCII.GetString(bytes.ToArray()); //convert bytes to JSON string
+        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg); //convert JSON to c# class
+
+        switch(header.cmd)
+        {
+            case Commands.HANDSHAKE:
+            HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
+            Debug.Log("Handshake message received!");
+            break;
+
+            case Commands.PLAYER_UPDATE:
+            PlayerUpdateMsg puMsg = JsonUtility.FromJson<PlayerUpdateMsg>(recMsg);
+            Debug.Log("Player update message received!");
+            break;
+
+            case Commands.SERVER_UPDATE:
+            ServerUpdateMsg suMsg = JsonUtility.FromJson<ServerUpdateMsg>(recMsg);
+            Debug.Log("Server update message received!");
+            break;
+
+            default:
+            Debug.Log("Unrecognized message received!");
+            break;
+        }
+    }
+
+    void OnDisconnect()
+    {
+        Debug.Log("Client got disconnected from server");
+        m_Connection = default(NetworkConnection);
+    }
+
+    void SendToServer(string message)
+    {
+        //When you establish a connection between the client and the server, 
+        //you send a data. The use of the BeginSend / EndSend pattern together with the DataStreamWriter,
+        //write data into the stream, and finally send it out on the network.
+
+        var writer = m_Driver.BeginSend(m_Connection);
+        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message), Allocator.Temp);
+        writer.WriteBytes(bytes);
+        m_Driver.EndSend(writer);
+    }
+
+    void Disconnect()
+    {
+        m_Connection.Disconnect(m_Driver);
+        m_Connection = default(NetworkConnection);
+    }
+
 }
--- Assets/Scripts/NetworkServer.cs
+++ Assets/Scripts/NetworkServer.cs
@@ -1,4 +1,12 @@
-﻿using UnityEngine;
+﻿/*
+Data from client to server:  (ReadBytes) -> bytes -> (GetString) -> JSON string -> (FromJson) -> c# class
+Data from server to client:   c# class -> (ToJson) -> JSON string -> (GetBytes) -> bytes -> (WriteBytes)
+  
+ */
+
+
+
+using UnityEngine;
 using UnityEngine.Assertions;
 using Unity.Collections;
 using Unity.Networking.Transport;
@@ -8,36 +16,101 @@ using System.Text;
 
 public class NetworkServer : MonoBehaviour
 {
-    public NetworkDriver m_Driver;
+    public NetworkDriver m_Driver; //similar to socket
     public ushort serverPort;
-    private NativeList<NetworkConnection> m_Connections;
+    private NativeList<NetworkConnection> m_Connections; //To hold our connections
 
     void Start ()
     {
-        m_Driver = NetworkDriver.Create();
+        m_Driver = NetworkDriver.Create(); // just makes sure you are creating your driver without any parameters.
         var endpoint = NetworkEndPoint.AnyIpv4;
         endpoint.Port = serverPort;
+
+        //Then we try to bind our driver to a specific network address and port, and if that does not fail, we call the Listen method.
+        //Important: the call to the Listen method sets the NetworkDriver to the Listen state.This means that the NetworkDriver will now actively listen for incoming connections.
         if (m_Driver.Bind(endpoint) != 0)
             Debug.Log("Failed to bind to port " + serverPort);
         else
             m_Driver.Listen();
 
+        //Finally we create a NativeList to hold all the connections.
         m_Connections = new NativeList<NetworkConnection>(16, Allocator.Persistent);
     }
 
-    void SendToClient(string message, NetworkConnection c){
-        var writer = m_Driver.BeginSend(NetworkPipeline.Null, c);
-        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message),Allocator.Temp);
-        writer.WriteBytes(bytes);
-        m_Driver.EndSend(writer);
-    }
     public void OnDestroy()
     {
+        //Both NetworkDriver and NativeList allocate unmanaged memory and need to be disposed. 
+        //To make sure this happens we can simply call the Dispose method when we are done with both of them.
         m_Driver.Dispose();
         m_Connections.Dispose();
     }
 
-    void OnConnect(NetworkConnection c){
+    void Update()
+    {
+        //As the com.unity.transport package uses the Unity C# Job System internally, 
+        //the m_Driver has a ScheduleUpdate method call. Inside our Update loop you need to make sure to call the Complete method on the JobHandle that is returned, 
+        //in order to know when you are ready to process any updates.
+        m_Driver.ScheduleUpdate().Complete();
+
+
+        //The first thing we want to do, after you have updated your m_Driver, is to handle your connections. 
+        //Start by cleaning up any old stale connections from the list before processing any new ones.
+        //This cleanup ensures that, when we iterate through the list to check what new events we have gotten, we dont have any old connections laying around.
+        //Inside the "Clean up connections" block below, we iterate through our connection list and just simply remove any stale connections.
+        // CleanUpConnections
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            if (!m_Connections[i].IsCreated)
+            {
+                m_Connections.RemoveAtSwapBack(i);
+                --i;
+            }
+        }
+
+
+        //Under "Accept new connections" below, we add a connection while there are new connections to accept.
+        // AcceptNewConnections
+        NetworkConnection c = m_Driver.Accept();
+        while (c != default(NetworkConnection)) //defulat(int, bool) will return 0, default(class, reference type) will return null, it's just checking null
+        {
+            OnConnect(c);
+
+            // Check if there is another new connection
+            c = m_Driver.Accept();
+        }
+
+
+        //Now we have an up-to-date connection list. You can now start querying the driver for events that might have happened since the last update.
+        // Read Incoming Messages
+        DataStreamReader stream; //This will be used in case any Data event was received. Then we just start looping through all our connections.
+        for (int i = 0; i < m_Connections.Length; i++)
+        {
+            Assert.IsTrue(m_Connections[i].IsCreated); //only when it's true
+
+            //For each connection we want to call PopEventForConnection while there are more events still needing to get processed.
+            NetworkEvent.Type cmd;
+            cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream); //Get data from connection
+            while (cmd != NetworkEvent.Type.Empty) //if cmd is not empty
+            {
+                //We are now ready to process events. Lets start with the Data event.
+                if (cmd == NetworkEvent.Type.Data)
+                {
+                    OnData(stream, i); //If it was data
+                }
+                //Finally, you need to handle the disconnect case. This is pretty straight forward, 
+                //if you receive a disconnect message you need to reset that connection to a default(NetworkConnection).
+                //As you might remember, the next time the Update loop runs you will clean up after yourself.
+                else if (cmd == NetworkEvent.Type.Disconnect)
+                {
+                    OnDisconnect(i);
+                }
+
+                cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
+            }
+        }
+    }
+    void OnConnect(NetworkConnection c)
+    {
         m_Connections.Add(c);
         Debug.Log("Accepted a connection");
 
@@ -47,13 +120,15 @@ public class NetworkServer : MonoBehaviour
         // SendToClient(JsonUtility.ToJson(m),c);        
     }
 
-    void OnData(DataStreamReader stream, int i){
-        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp);
-        stream.ReadBytes(bytes);
-        string recMsg = Encoding.ASCII.GetString(bytes.ToArray());
-        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg);
+    void OnData(DataStreamReader stream, int i)
+    {
+        NativeArray<byte> bytes = new NativeArray<byte>(stream.Length,Allocator.Temp); //Where to store bytes data
+        stream.ReadBytes(bytes); //Get bytes data from stream
+        string recMsg = Encoding.ASCII.GetString(bytes.ToArray()); //convert bytes data to string(JSON)
+        NetworkHeader header = JsonUtility.FromJson<NetworkHeader>(recMsg); //convert JSON to c# object
 
-        switch(header.cmd){
+        switch(header.cmd)
+        {
             case Commands.HANDSHAKE:
             HandshakeMsg hsMsg = JsonUtility.FromJson<HandshakeMsg>(recMsg);
             Debug.Log("Handshake message received!");
@@ -72,58 +147,26 @@ public class NetworkServer : MonoBehaviour
         }
     }
 
-    void OnDisconnect(int i){
+    void OnDisconnect(int i)
+    {
         Debug.Log("Client disconnected from server");
         m_Connections[i] = default(NetworkConnection);
     }
 
-    void Update ()
+
+    void SendToClient(string message, NetworkConnection c)
     {
-        m_Driver.ScheduleUpdate().Complete();
+        //To send anything with the NetworkDriver we need a instance of a DataStreamWriter.
+        //A DataStreamWriter is a new type that comes with the com.unity.transport package. 
+        //You get a DataStreamWriter when you start sending a message by calling BeginSend.
 
-        // CleanUpConnections
-        for (int i = 0; i < m_Connections.Length; i++)
-        {
-            if (!m_Connections[i].IsCreated)
-            {
+        var writer = m_Driver.BeginSend(NetworkPipeline.Null, c); //One thing to note here is that we are NetworkPipeline.Null, to the BeginSend function. This way we say to the driver to use the unreliable pipeline to send our data. It is also possible to not specify a pipeline.
+        NativeArray<byte> bytes = new NativeArray<byte>(Encoding.ASCII.GetBytes(message), Allocator.Temp);
+        writer.WriteBytes(bytes);
 
-                m_Connections.RemoveAtSwapBack(i);
-                --i;
-            }
-        }
-
-        // AcceptNewConnections
-        NetworkConnection c = m_Driver.Accept();
-        while (c  != default(NetworkConnection))
-        {            
-            OnConnect(c);
-
-            // Check if there is another new connection
-            c = m_Driver.Accept();
-        }
-
-
-        // Read Incoming Messages
-        DataStreamReader stream;
-        for (int i = 0; i < m_Connections.Length; i++)
-        {
-            Assert.IsTrue(m_Connections[i].IsCreated);
-            
-            NetworkEvent.Type cmd;
-            cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
-            while (cmd != NetworkEvent.Type.Empty)
-            {
-                if (cmd == NetworkEvent.Type.Data)
-                {
-                    OnData(stream, i);
-                }
-                else if (cmd == NetworkEvent.Type.Disconnect)
-                {
-                    OnDisconnect(i);
-                }
-
-                cmd = m_Driver.PopEventForConnection(m_Connections[i], out stream);
-            }
-        }
+        //After you have written your data to your stream, you call the EndSend method on the driver and off it goes:
+        m_Driver.EndSend(writer);
     }
+
+
 }
